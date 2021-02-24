ea71d961ce4b2ffe4010b3822b6b6f49983f2d96
chaeyoung <codud7239@gmail.com>
2021-02-24 22:02:40 +0900

    [feat] CartsSaveRequestDto 추가

---
+++ src/main/java/com/team_project/shop/network/request/CartsSaveRequestDto.java
@@ -0,0 +1,35 @@
+package com.team_project.shop.network.request;
+
+import com.team_project.shop.config.ProductStateAttribueConverter;
+import com.team_project.shop.domain.cart.Carts;
+import com.team_project.shop.domain.product.Images;
+import com.team_project.shop.domain.product.Product_Options;
+import com.team_project.shop.domain.product.Products;
+import com.team_project.shop.domain.user.Users;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+public class CartsSaveRequestDto {
+
+    private Long quantity;
+
+    @Builder
+    public CartsSaveRequestDto(Long quantity){
+        this.quantity = quantity;
+    }
+
+    public Carts toEntity(Users user, Product_Options productOptions){
+        return Carts.builder()
+                .user(user)
+                .productOption(productOptions)
+                .quantity(this.quantity)
+                .build();
+    }
+
+
+}
