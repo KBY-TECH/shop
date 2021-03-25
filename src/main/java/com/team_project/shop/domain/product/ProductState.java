8d46022ccf5d39e650c380e22005941f5df72730
niveus323 <niveus323@gmail.com>
2021-03-26 02:18:54 +0900

    [feat] ProductState Enum 클래스 value 기반 find, value 출력 함수 추가

--- src/main/java/com/team_project/shop/domain/product/ProductState.java
+++ src/main/java/com/team_project/shop/domain/product/ProductState.java
@@ -3,15 +3,24 @@ package com.team_project.shop.domain.product;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
+import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
 public enum ProductState {
-    ONSALE,
-    SUSPENSION,
-    OUTOFSTOCK;
+    ONSALE("판매 중"),
+    SUSPENSION("판매중지"),
+    OUTOFSTOCK("품절");
 
-
+    private final String text;
+    //string을 넣으면 enum값을 리턴
+    public static ProductState find(String state){
+        return Arrays.stream(values())
+                .filter(obj -> obj.text.equals(state))
+                .findFirst()
+                .orElse(ONSALE);
+    }
+    public String toText() {
+        return this.text;
+    }
 }
