896a82fb3e8858ffffcf857a6042fba933b5cda6
niveus323 <niveus323@gmail.com>
2021-04-08 00:56:41 +0900

    [update] 판매자 페이지 UI 조정, 미사용 html 제거

--- src/main/resources/static/js/seller.js
+++ src/main/resources/static/js/seller.js
@@ -10,26 +10,45 @@ boxes.change(function(){
     var checkedAll = (boxLength === checkedLength);
     checkAll.prop('checked',checkedAll);
 });
+let btnStop = $('.btn-stop');
+btnStop.click(function(){
+    $('[name="check"]:checked').each(function(){
+        var tr = $(this).closest('tr');
+        //stock이 0이하일경우 알람을 띄우고 '품절'이라고 표시하기.
+        if( parseInt(tr.children('[name="stock"]').val()) <= 0){
+            tr.children('.state').text(function(i,oldText){
+                return oldText === '판매중지'? '품절' : '판매중지';
+            });
+        }
+        else{
+            tr.children('.state').text(function(i,oldText){
+                return oldText === '판매중지'? '판매 중' : '판매중지';
+            });
+        }
+
+
+    })
+});
 
 //상품등록(가방)
 var post = {
-    init : function(){
+    init : function(type){
         var _this = this;
         $("#btn-save").on("click",function(){
-            _this.save();
+            _this.save(type);
         });
 
         $("#btn-update").on("click",function(){
-            _this.update();
+            _this.update(type);
         });
     },
-    save : function(){
+    save : function(type){
         var formData = new FormData();
         makeProductVO(formData);
 
         $.ajax({
             type : 'POST',
-            url : "/api/product/bag",
+            url : "/api/product/"+type,
             enctype : "multipart/form-data",
             data : formData,
             processData : false,
@@ -42,7 +61,7 @@ var post = {
         });
 
     },
-    update : function(){
+    update : function(type){
         var formData = new FormData();
         // formData.append("productName",$('[name=productName]').val());
         //판매상태 처리, 상품 ID 받기 추가해야함
@@ -51,7 +70,7 @@ var post = {
         makeProductVO(formData);
         $.ajax({
             type : 'PUT',
-            url : "/api/product/bag/"+id,
+            url : "/api/product/"+type+"/"+id,
             enctype : "multipart/form-data",
             data : formData,
             processData : false,
@@ -65,7 +84,7 @@ var post = {
         });
     }
 };
-post.init();
+
 
 function makeProductVO(formData){
     formData.append("productName",$('[name=productName]').val());
@@ -120,6 +139,51 @@ function makeProductVO(formData){
     $('[name="afterServiceAddress"]').each(function(){
         formData.append("afterServiceAddress",$(this).val());
     });
+    $('[name="manufacturedDate"]').each(function(){
+        formData.append("manufacturedDate",$(this).val());
+    });
+    $('[name="foodType"]').each(function(){
+        formData.append("foodType",$(this).val());
+    });
+    $('[name="qualityMaintenanceDate"]').each(function(){
+        formData.append("qualityMaintenanceDate",$(this).val());
+    });
+    $('[name="capacityByPackingUnit"]').each(function(){
+        formData.append("capacityByPackingUnit",$(this).val());
+    });
+    $('[name="materialContent"]').each(function(){
+        formData.append("materialContent",$(this).val());
+    });
+    $('[name="nutritionalIngredients"]').each(function(){
+        formData.append("nutritionalIngredients",$(this).val());
+    });
+    $('[name="geneticallyModified"]').each(function(){
+        formData.append("geneticallyModified",$(this).val());
+    });
+    $('[name="importedFood"]').each(function(){
+        formData.append("importedFood",$(this).val());
+    });
+    $('[name="consumerCounselingPhoneNum"]').each(function(){
+        formData.append("consumerCounselingPhoneNum",$(this).val());
+    });
+    $('[name="meatPart"]').each(function(){
+        formData.append("meatPart",$(this).val());
+    });
+    $('[name="origin"]').each(function(){
+        formData.append("origin",$(this).val());
+    });
+    $('[name="indication"]').each(function(){
+        formData.append("indication",$(this).val());
+    });
+    $('[name="composition"]').each(function(){
+        formData.append("composition",$(this).val());
+    });
+    $('[name="storageMethod"]').each(function(){
+        formData.append("storageMethod",$(this).val());
+    });
+    $('[name="foodName"]').each(function(){
+        formData.append("foodName",$(this).val());
+    });
 }
 function makeBagDto(formData){
     formData.append("productName",$('[name=productName]').val());
@@ -148,8 +212,29 @@ function addRow(rows){
         "<td><input type=\"file\" name=\"mainImage\" accept=\"image/jpeg, image/png\" value=\"등록\"></td>" +
         "<td><input type=\"file\" name=\"detailImage\" accept=\"image/jpeg, image/png\" value=\"등록\"></td>" +
         "<td><input type=\"button\" class=\"btn-information\" value=\"등록\" onclick=\"openInformation(this)\"></td></tr>";
-
     $('#options').append(newRow);
+
+}
+function addFrozenfood(rows){
+    let newInformation = "<div class='hover-information "+rows+"'>" +
+        "<span class=\"helper\"></span>" +
+        "<div class=\"information\"><p>아이템의 고시정보를 입력하세요.</p>" +
+        "<table border=\"1\">" +
+        "<tr align=\"center\"><td>고시정보명</td><td>내용</td></tr>" +
+        "<tr align=\"center\"><td>식품의 유형</td><td><textarea name=\"foodType\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>생산자 및 소재지</td><td><textarea name=\"producer\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>제조연월일, 유통기한, 또는 품질유지기한</td><td><textarea name=\"qualityMaintenanceDate\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>포장단위별 내용물의 용량(중량), 수량</td><td><textarea name=\"capacityByPackingUnit\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>원재료명 및 함량</td><td><textarea name=\"materialContent\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>영양성분</td><td><textarea name=\"nutritionalIngredients\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>유전자변형식품에 해당하는 경우의 표시</td><td><textarea name=\"geneticallyModified\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>소비자안전을 위한 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>수입식품 문구</td><td><textarea name=\"importedFood\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>소비자상담관련 전화번호</td><td><textarea name=\"consumerCounselingPhoneNum\"></textarea></td></tr>" +
+        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
+    $("body").append(newInformation);
+}
+function addBag(rows){
     let newInformation = "<div class='hover-information "+rows+"'>" +
         "<span class=\"helper\"></span>" +
         "<div class=\"information\"><p>아이템의 고시정보를 입력하세요.</p>" +
@@ -162,13 +247,86 @@ function addRow(rows){
         "<tr align=\"center\"><td>제조자(수입자)</td><td><textarea name=\"producer\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>제조국</td><td><textarea name=\"madeIn\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>취급시 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
-        "<tr align=\"center\">" +
-        "<td>품질보증기준</td><td><textarea name=\"qualityAssuranceStandard\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>품질보증기준</td><td><textarea name=\"qualityAssuranceStandard\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>A/S 책임자와 전화번호</td><td><textarea name=\"afterServiceAddress\"></textarea></td></tr>" +
         "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
     $("body").append(newInformation);
 }
-
+function addMeat(rows){
+    let newInformation = "<div class='hover-information "+rows+"'>" +
+        "<span class=\"helper\"></span>" +
+        "<div class=\"information\"><p>아이템의 고시정보를 입력하세요.</p>" +
+        "<table border=\"1\">" +
+        "<tr align=\"center\"><td>고시정보명</td><td>내용</td></tr>" +
+        "<tr align=\"center\"><td>품목 또는 명칭</td><td><textarea name=\"meatPart\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>포장단위별 내용물의 용량(중량),수량,크기</td><td><textarea name=\"capacityByPackingUnit\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>생산자(수입자)</td><td><textarea name=\"producer\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>원산지</td><td><textarea name=\"origin\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>제조연월일,유통기한</td><td><textarea name=\"qualityMaintenanceDate\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>관련법상 표시사항</td><td><textarea name=\"indication\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>수입식품 문구 여부</td><td><textarea name=\"importedFood\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>상품구성</td><td><textarea name=\"composition\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>보관방법,취급방법</td><td><textarea name=\"storageMethod\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>소비자안전을 위한 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>소비자상담관련 전화번호</td><td><textarea name=\"consumerCounselingPhoneNum\"></textarea></td></tr>" +
+        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
+    $("body").append(newInformation);
+}
+function addSeafood(rows){
+    let newInformation = "<div class='hover-information "+rows+"'>" +
+        "<span class=\"helper\"></span>" +
+        "<div class=\"information\"><p>아이템의 고시정보를 입력하세요.</p>" +
+        "<table border=\"1\">" +
+        "<tr align=\"center\"><td>고시정보명</td><td>내용</td></tr>" +
+        "<tr align=\"center\"><td>제품명</td><td><textarea name=\"foodName\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>식품의 유형</td><td><textarea name=\"foodType\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>생산자 및 소재지</td><td><textarea name=\"producer\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>제조연월일, 유통기한, 또는 품질유지기한</td><td><textarea name=\"qualityMaintenanceDate\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>포장단위별 내용물의 용량(중량), 수량</td><td><textarea name=\"capacityByPackingUnit\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>원재료명 및 함량</td><td><textarea name=\"materialContent\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>영양성분</td><td><textarea name=\"nutritionalIngredients\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>유전자변형식품에 해당하는 경우의 표시</td><td><textarea name=\"geneticallyModified\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>소비자안전을 위한 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>수입식품 문구</td><td><textarea name=\"importedFood\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>소비자상담관련 전화번호</td><td><textarea name=\"consumerCounselingPhoneNum\"></textarea></td></tr>" +
+        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
+    $("body").append(newInformation);
+}
+function addClothes(rows){
+    let newInformation = "<div class='hover-information "+rows+"'>" +
+        "<span class=\"helper\"></span>" +
+        "<div class=\"information\"><p>아이템의 고시정보를 입력하세요.</p>" +
+        "<table border=\"1\">" +
+        "<tr align=\"center\"><td>고시정보명</td><td>내용</td></tr>" +
+        "<tr align=\"center\"><td>제품 소재</td><td><textarea name=\"material\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>색상</td><td><textarea name=\"color\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>치수</td><td><textarea name=\"size\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>제조자(수입자)</td><td><textarea name=\"producer\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>제조국</td><td><textarea name=\"madeIn\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>취급시 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>제조연월</td><td><textarea name=\"manufacturedDate\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>품질보증기준</td><td><textarea name=\"qualityAssuranceStandard\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>A/S 책임자와 전화번호</td><td><textarea name=\"afterServiceAddress\"></textarea></td></tr>" +
+        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
+    $("body").append(newInformation);
+}
+function addWallet(rows){
+    let newInformation = "<div class='hover-information "+rows+"'>" +
+        "<span class=\"helper\"></span>" +
+        "<div class=\"information\"><p>아이템의 고시정보를 입력하세요.</p>" +
+        "<table border=\"1\">" +
+        "<tr align=\"center\"><td>고시정보명</td><td>내용</td></tr>" +
+        "<tr align=\"center\"><td>종류</td><td><textarea name=\"kind\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>소재</td><td><textarea name=\"material\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>크기</td><td><textarea name=\"size\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>제조자(수입자)</td><td><textarea name=\"producer\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>제조국</td><td><textarea name=\"madeIn\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>취급시 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>품질보증기준</td><td><textarea name=\"qualityAssuranceStandard\"></textarea></td></tr>" +
+        "<tr align=\"center\"><td>A/S 책임자와 전화번호</td><td><textarea name=\"afterServiceAddress\"></textarea></td></tr>" +
+        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
+    $("body").append(newInformation);
+}
 function openInformation(e){
     let className = $(e).closest('tr').attr('class');
     $('.hover-information').each(function (){
