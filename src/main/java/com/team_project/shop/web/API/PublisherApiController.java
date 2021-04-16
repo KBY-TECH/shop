d1a63763362ce54cd65f776c514e45a5adf6b5ea
KBY-TECH <soccer_by@naver.com>
2021-04-16 18:11:35 +0900

    [update] : Publisher Test

--- src/main/java/com/team_project/shop/web/API/PublisherApiController.java
+++ src/main/java/com/team_project/shop/web/API/PublisherApiController.java
@@ -1,5 +1,6 @@
 package com.team_project.shop.web.API;
 
+import com.team_project.shop.Service.IFS.Publisher_IFS;
 import com.team_project.shop.network.request.PublisherCreateRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,16 +20,31 @@ import javax.validation.constraints.NotEmpty;
 @RequestMapping("/api/publisher")
 @RestController
 public class PublisherApiController {
+    private final Publisher_IFS publisher;
     @PostMapping("")
     public ResponseEntity<HttpStatus> publisherCreate(@Valid @RequestBody PublisherCreateRequestDto requestDto)
     {
-        return null;
+        return publisher.signUp(requestDto);
     }
 
-    @PostMapping("/businessNumberCheck")
-    public ResponseEntity<HttpStatus> businessNumberCheck(@RequestParam(value = "business_number") @Valid @NotEmpty String businessNumber)
+    @PostMapping("/emailCheck")
+    public ResponseEntity emailCheck(@RequestParam(value ="email") @Valid @Email @NotEmpty String email)
     {
-        return null;
+        if(publisher.isDuplicateEmail(email))
+        {
+            return ResponseEntity.ok("사용가능한 이메일 입니다.");
+        }
+        return new ResponseEntity("중복된 이메일 입니다.",HttpStatus.CONFLICT);
     }
+    @PostMapping("/businessNumberCheck")
+    public ResponseEntity businessNumberCheck(@RequestParam(value = "business_number") @Valid @NotEmpty String businessNumber)
+    {
+        if(publisher.isDuplicateBusinessNumber(businessNumber))
+        {
+            return ResponseEntity.ok("사용가능한 사업자 번호 입니다.");
+        }
+        return new ResponseEntity("중복된 사업자 번호 입니다.",HttpStatus.CONFLICT);
+    }
+
 
 }
