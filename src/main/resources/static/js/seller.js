5f9c17c6430cf83ae273895b066c83a2bd9ed392
niveus323 <niveus323@gmail.com>
2021-04-19 20:50:05 +0900

    [update] 판매 등록/수정 및 상품 페이지 업데이트

--- src/main/resources/static/js/seller.js
+++ src/main/resources/static/js/seller.js
@@ -13,205 +13,28 @@ boxes.change(function(){
 let btnStop = $('.btn-stop');
 btnStop.click(function(){
     $('[name="check"]:checked').each(function(){
-        var tr = $(this).closest('tr');
+        let tr = $(this).closest('tr');
+        let stateInput = tr.find('[name="state"]');
+        let prevState = stateInput.val();
         //stock이 0이하일경우 알람을 띄우고 '품절'이라고 표시하기.
-        if( parseInt(tr.children('[name="stock"]').val()) <= 0){
-            tr.children('.state').text(function(i,oldText){
-                return oldText === '판매중지'? '품절' : '판매중지';
-            });
+        if( parseInt(tr.find('[name="stock"]').val()) <= 0){
+            stateInput.val(prevState === '판매중지'? '품절' : '판매중지');
         }
         else{
-            tr.children('.state').text(function(i,oldText){
-                return oldText === '판매중지'? '판매 중' : '판매중지';
-            });
+            stateInput.val(prevState === '판매중지'? '판매 중' : '판매중지');
         }
-
-
-    })
+    });
 });
-
-//상품등록(가방)
-var post = {
-    init : function(type){
-        var _this = this;
-        $("#btn-save").on("click",function(){
-            _this.save(type);
-        });
-
-        $("#btn-update").on("click",function(){
-            _this.update(type);
-        });
-    },
-    save : function(type){
-        var formData = new FormData();
-        makeProductVO(formData);
-
-        $.ajax({
-            type : 'POST',
-            url : "/api/product/"+type,
-            enctype : "multipart/form-data",
-            data : formData,
-            processData : false,
-            contentType : false,
-        }).done(function(){
-            alert("등록완료");
-            window.location.href = '/api/product/seller/user';
-        }).fail(function(error){
-            alert("등록실패");
-        });
-
-    },
-    update : function(type){
-        var formData = new FormData();
-        // formData.append("productName",$('[name=productName]').val());
-        //판매상태 처리, 상품 ID 받기 추가해야함
-        let id = $("#product-id").val();
-
-        makeProductVO(formData);
-        $.ajax({
-            type : 'PUT',
-            url : "/api/product/"+type+"/"+id,
-            enctype : "multipart/form-data",
-            data : formData,
-            processData : false,
-            contentType : false,
-        }).done(function(){
-            alert("등록완료");
-            window.location.href = '/api/product/seller/user';
-            window.location.method = 'GET';
-        }).fail(function(error){
-            alert("등록실패");
-        });
-    }
-};
-
-
-function makeProductVO(formData){
-    formData.append("productName",$('[name=productName]').val());
-    $('[name="optionName"]').each(function(){
-        formData.append("optionName",$(this).val());
-    });
-    $('[name="price"]').each(function(){
-        formData.append("price",$(this).val());
-    });
-    $('[name="stock"]').each(function(){
-        formData.append("stock",$(this).val());
-    });
-    $('[name="mainImage"]').each(function(){
-        if($(this).prop('files').length > 0){
-            formData.append("mainImage",$(this).prop('files')[0]);
-            formData.append("mainImageKey",$(this).closest('tr').attr('class'));
-        }
-    });
-    $('[name="detailImage"]').each(function(){
-        if($(this).prop('files').length > 0){
-            formData.append("detailImage",$(this).prop('files')[0]);
-            formData.append("detailImageKey",$(this).closest('tr').attr('class'));
-        }
-    });
-    $('.state').each(function(){
-        formData.append('state',$(this).text());
-    });
-    $('[name="kind"]').each(function(){
-        formData.append("kind",$(this).val());
-    });
-    $('[name="material"]').each(function(){
-        formData.append("material",$(this).val());
-    });
-    $('[name="size"]').each(function(){
-        formData.append("size",$(this).val());
-    });
-    $('[name="color"]').each(function(){
-        formData.append("color",$(this).val());
-    });
-    $('[name="producer"]').each(function(){
-        formData.append("producer",$(this).val());
-    });
-    $('[name="madeIn"]').each(function(){
-        formData.append("madeIn",$(this).val());
-    });
-    $('[name="precautions"]').each(function(){
-        formData.append("precautions",$(this).val());
-    });
-    $('[name="qualityAssuranceStandard"]').each(function(){
-        formData.append("qualityAssuranceStandard",$(this).val());
-    });
-    $('[name="afterServiceAddress"]').each(function(){
-        formData.append("afterServiceAddress",$(this).val());
-    });
-    $('[name="manufacturedDate"]').each(function(){
-        formData.append("manufacturedDate",$(this).val());
-    });
-    $('[name="foodType"]').each(function(){
-        formData.append("foodType",$(this).val());
-    });
-    $('[name="qualityMaintenanceDate"]').each(function(){
-        formData.append("qualityMaintenanceDate",$(this).val());
-    });
-    $('[name="capacityByPackingUnit"]').each(function(){
-        formData.append("capacityByPackingUnit",$(this).val());
-    });
-    $('[name="materialContent"]').each(function(){
-        formData.append("materialContent",$(this).val());
-    });
-    $('[name="nutritionalIngredients"]').each(function(){
-        formData.append("nutritionalIngredients",$(this).val());
-    });
-    $('[name="geneticallyModified"]').each(function(){
-        formData.append("geneticallyModified",$(this).val());
-    });
-    $('[name="importedFood"]').each(function(){
-        formData.append("importedFood",$(this).val());
-    });
-    $('[name="consumerCounselingPhoneNum"]').each(function(){
-        formData.append("consumerCounselingPhoneNum",$(this).val());
-    });
-    $('[name="meatPart"]').each(function(){
-        formData.append("meatPart",$(this).val());
-    });
-    $('[name="origin"]').each(function(){
-        formData.append("origin",$(this).val());
-    });
-    $('[name="indication"]').each(function(){
-        formData.append("indication",$(this).val());
-    });
-    $('[name="composition"]').each(function(){
-        formData.append("composition",$(this).val());
-    });
-    $('[name="storageMethod"]').each(function(){
-        formData.append("storageMethod",$(this).val());
-    });
-    $('[name="foodName"]').each(function(){
-        formData.append("foodName",$(this).val());
-    });
-}
-function makeBagDto(formData){
-    formData.append("productName",$('[name=productName]').val());
-    $('.default').each(function(){
-        var name = $(this).attr("name");
-        $("[name="+name+"]").each(function(){
-            if($(this).attr('type') !== "file"){
-                formData.append(name,$(this).val());
-            }else if($(this).prop('files').length > 0){
-                formData.append(name, $(this).prop('files')[0]);
-                formData.append(name+"Key", $(this).closest('tr').attr('class'));
-            }
-        });
-    });
-    $('.state').each(function(){
-        formData.append('state',$(this).text());
-    });
-}
 function addRow(rows){
     let newRow ="<tr class="+rows+" align='center'>"+
         "<td><input type=\"checkbox\" name=\"check\"></td>" +
         "<td><input type=\"text\" name=\"optionName\"></td>" +
-        "<td class=\"state-new\">새 상품</td>" +
+        "<td ><input type=\"text\" name=\"state\" value=\"새 상품\" readonly></td>" +
         "<td><input type=\"text\" name=\"price\" oninput=\"this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\\..*)\\./g, '$1');\" /></td>" +
         "<td><input type=\"text\" name=\"stock\" oninput=\"this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\\..*)\\./g, '$1');\" /></td>" +
-        "<td><input type=\"file\" name=\"mainImage\" accept=\"image/jpeg, image/png\" value=\"등록\"></td>" +
-        "<td><input type=\"file\" name=\"detailImage\" accept=\"image/jpeg, image/png\" value=\"등록\"></td>" +
-        "<td><input type=\"button\" class=\"btn-information\" value=\"등록\" onclick=\"openInformation(this)\"></td></tr>";
+        "<td><button class='input-required btn btn-default' type=\"button\"><label for=\"mainImage"+rows+"\">등록</label></button><input id=\"mainImage"+rows+"\" class=\"mainImage\" type=\"file\" name=\"mainImage\" accept=\"image/jpeg, image/png\" style=\"display: none\" onchange=\"imageUpdated(this)\"><input type=\"hidden\" id=\"mainImageKey"+rows+"\" name=\"mainImageKey\" disabled></td>" +
+        "<td><button class='input-required btn btn-default' type=\"button\"><label for=\"detailImage"+rows+"\">등록</label></button><input id=\"detailImage"+rows+"\" class=\"detailImage\" type=\"file\" name=\"detailImage\" accept=\"image/jpeg, image/png\" style=\"display: none\" onchange=\"imageUpdated(this)\"><input type=\"hidden\"  id=\"detailImageKey"+rows+"\" name=\"detailImageKey\" disabled></td>" +
+        "<td><button class=\"btn-information input-required btn btn-default\" type=\"button\" onclick=\"openInformation(this)\"><label>등록</label></button></td>";
     $('#options').append(newRow);
 
 }
@@ -231,8 +54,8 @@ function addFrozenfood(rows){
         "<tr align=\"center\"><td>소비자안전을 위한 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>수입식품 문구</td><td><textarea name=\"importedFood\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>소비자상담관련 전화번호</td><td><textarea name=\"consumerCounselingPhoneNum\"></textarea></td></tr>" +
-        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
-    $("body").append(newInformation);
+        "</table><button type=\"button\" class=\"btn btn-default\" onclick=\"closeInformation(this)\">등록하기</button></div></div>";
+    $("form").append(newInformation);
 }
 function addBag(rows){
     let newInformation = "<div class='hover-information "+rows+"'>" +
@@ -249,8 +72,8 @@ function addBag(rows){
         "<tr align=\"center\"><td>취급시 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>품질보증기준</td><td><textarea name=\"qualityAssuranceStandard\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>A/S 책임자와 전화번호</td><td><textarea name=\"afterServiceAddress\"></textarea></td></tr>" +
-        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
-    $("body").append(newInformation);
+        "</table><button type=\"button\" class=\"btn btn-default\" onclick=\"closeInformation(this)\">등록하기</button></div></div>";
+    $("form").append(newInformation);
 }
 function addMeat(rows){
     let newInformation = "<div class='hover-information "+rows+"'>" +
@@ -269,8 +92,8 @@ function addMeat(rows){
         "<tr align=\"center\"><td>보관방법,취급방법</td><td><textarea name=\"storageMethod\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>소비자안전을 위한 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>소비자상담관련 전화번호</td><td><textarea name=\"consumerCounselingPhoneNum\"></textarea></td></tr>" +
-        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
-    $("body").append(newInformation);
+        "</table><button type=\"button\" class=\"btn btn-default\" onclick=\"closeInformation(this)\">등록하기</button></div></div>";
+    $("form").append(newInformation);
 }
 function addSeafood(rows){
     let newInformation = "<div class='hover-information "+rows+"'>" +
@@ -289,8 +112,8 @@ function addSeafood(rows){
         "<tr align=\"center\"><td>소비자안전을 위한 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>수입식품 문구</td><td><textarea name=\"importedFood\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>소비자상담관련 전화번호</td><td><textarea name=\"consumerCounselingPhoneNum\"></textarea></td></tr>" +
-        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
-    $("body").append(newInformation);
+        "</table><button type=\"button\" class=\"btn btn-default\" onclick=\"closeInformation(this)\">등록하기</button></div></div>";
+    $("form").append(newInformation);
 }
 function addClothes(rows){
     let newInformation = "<div class='hover-information "+rows+"'>" +
@@ -307,8 +130,8 @@ function addClothes(rows){
         "<tr align=\"center\"><td>제조연월</td><td><textarea name=\"manufacturedDate\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>품질보증기준</td><td><textarea name=\"qualityAssuranceStandard\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>A/S 책임자와 전화번호</td><td><textarea name=\"afterServiceAddress\"></textarea></td></tr>" +
-        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
-    $("body").append(newInformation);
+        "</table><button type=\"button\" class=\"btn btn-default\" onclick=\"closeInformation(this)\">등록하기</button></div></div>";
+    $("form").append(newInformation);
 }
 function addWallet(rows){
     let newInformation = "<div class='hover-information "+rows+"'>" +
@@ -324,21 +147,62 @@ function addWallet(rows){
         "<tr align=\"center\"><td>취급시 주의사항</td><td><textarea name=\"precautions\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>품질보증기준</td><td><textarea name=\"qualityAssuranceStandard\"></textarea></td></tr>" +
         "<tr align=\"center\"><td>A/S 책임자와 전화번호</td><td><textarea name=\"afterServiceAddress\"></textarea></td></tr>" +
-        "</table><input type=\"button\" value=\"등록하기\" onclick=\"closeInformation()\"></div></div>";
-    $("body").append(newInformation);
+        "</table><button type=\"button\" class=\"btn btn-default\" onclick=\"closeInformation(this)\">등록하기</button></div></div>";
+    $("form").append(newInformation);
 }
+let informButton;
 function openInformation(e){
     let className = $(e).closest('tr').attr('class');
     $('.hover-information').each(function (){
-        if($(this).hasClass(className)) $(this).show();
+        if($(this).hasClass(className)){
+            $(this).show();
+            informButton = $(e);
+        }
         else $(this).hide();
     })
 }
-function closeInformation(){
+function closeInformation(e){
     $(".hover-information").hide();
+    let isInformationNull = false;
+    $(e).closest('.information').find('textarea').each(function(){;
+        if(!$(this).val()){
+            isInformationNull = true;
+        }
+    });
+    if(!isInformationNull){
+        informButton.removeClass('btn-default');
+        informButton.addClass('btn-success');
+        informButton.children('label').text('변경');
+    }
+    else{
+        informButton.removeClass('btn-success');
+        informButton.addClass('btn-default');
+        informButton.children('label').text('등록');
+    }
 }
 
-function stopProduct(){
-    //체크된 tr의 상품 ID를 받고 컨트롤러에 요청하여 해당 상품의 옵션들이 전부 판매중지상태가 되도록 한다.
-    console.log("판매 멈춰!");
+function imageUpdated(e){
+    let optionNum = $(e).closest('tr').attr('class');
+    let className = $(e).attr('class')+'Key'+optionNum;
+    $('#'+className).prop('disabled',false).val(optionNum);
+    $(e).off('change');
+    let labelButton = $(e).closest('td').children('button');
+    labelButton.removeClass('btn-default');
+    labelButton.addClass('btn-success');
+    labelButton.children('label').text('변경');
+}
+
+function beforeSubmit(){
+    let result = true;
+    $('.input-required').each(function(){
+        if(!$(this).hasClass('btn-success')){
+            let btn = $(this);
+            btn.addClass('warning');
+            setTimeout(function() {
+                btn.removeClass('warning');
+                },6000);
+            result = false;
+        }
+    })
+    return result;
 }
